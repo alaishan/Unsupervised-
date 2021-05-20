30d64298cbe6cd4556998bf952c1a4e5a436c47e
Alaisha Naidu <69754098+alaishan@users.noreply.github.com>
2021-05-20 10:59:12 +0200

    Transforming Features using sklearn pipeline

--- 3.TransformingFeatures.py
+++ 3.TransformingFeatures.py
@@ -4,7 +4,7 @@
 Created on Tue May 18 15:19:51 2021
 
 @author: Alaisha Naidu
-Name: Transforming Features 
+Name: Transforming Features Pipeline
 Creds: DataCamp
 Dataset: Piedmont Wines Dataset from Italy
 
@@ -16,14 +16,18 @@ from sklearn.datasets import load_wine
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+from sklearn.pipeline import make_pipeline
 
 wine = load_wine()
 samples = wine.data
 
-model = KMeans(n_clusters = 3) #3 clusters becasue there are 3 varieties of wines
-model.fit(samples) #fits the model to the data by locating and remembering the regions where the different clusters occur
-labels = model.predict(samples) #returns a cluster lable for each sample, indicated to which cluster a sample belongs 
-print(model.inertia_) #use after fit
+scaler = StandardScaler() #gives every feature mean 0 and variance 1
+kmeans = KMeans(n_clusters = 3) #3 clusters becasue there are 3 varieties of wines
+pipeline = make_pipeline(scaler, kmeans)
+pipeline.fit(samples) #fits the model to the data by locating and remembering the regions where the different clusters occur
+
+labels = pipeline.predict(samples) #returns a cluster lable for each sample, indicated to which cluster a sample belongs 
+print(kmeans.inertia_) #use after fit
 #K-Means automatically generates clusters that minimizes the cluster inertia 
 #print(labels)
 
@@ -35,7 +39,7 @@ plt.show()
 names = wine.target_names
 df = pd.DataFrame(samples, columns=wine.feature_names)
 df['varieties'] = wine.target
-df['varieties'] = df['varieties'].replace(to_replace= [0, 1, 2], value = ['barbera', 'barolo', 'grignolino']) ### SUPER IMPORTANT - REPLACES 0,1,2 with variety name
+df['varieties'] = df['varieties'].replace(to_replace= [0, 1, 2], value = ['barbera','grignolino', 'barolo']) ### SUPER IMPORTANT - REPLACES 0,1,2 with variety name
 varieties = df['varieties'].values.tolist()
 
 
@@ -50,11 +54,12 @@ print(ct)
 #features of wine dataset have very different variances - variance of a feature measures spead of its values
 #In K-Means clustering, the variance of a feature corresponds to its influence on the clustering algorithm 
 #To give every feature an even chance, the data must be transformed so that the features all have an equal variance
-scaler = StandardScaler() #gives every feature mean 0 and variance 1
+
 scaler.fit(samples)
 StandardScaler(copy=True, with_mean=True, with_std=True)
 samples_scaled= scaler.transform(samples)
 
+#StandSCaler transforms the data using the transform method while K-Means assigns cluster labels to samples using predict method
 
 """
 Output: can see that the clusters are very mixed!
@@ -72,11 +77,22 @@ Output: can see that the clusters are very mixed!
 176       0  grignolino
 177       2  grignolino
 
+Before standardization: 
+
 [178 rows x 2 columns]
 varieties  barbera  barolo  grignolino
 labels                                
-0               13      20          29
-1               46       1           0
-2                0      50          19
+0               29      13          20
+1                0      46           1
+2               19       0          50
+
+After standardization:
+
+[178 rows x 2 columns]
+varieties  barbera  barolo  grignolino
+labels                                
+0                0      59           3
+1               48       0           3
+2                0       0          65
 
 """
