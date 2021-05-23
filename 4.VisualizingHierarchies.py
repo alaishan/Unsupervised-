e569b1cf6c152757456d889e9de30487244fa291
Alaisha Naidu <69754098+alaishan@users.noreply.github.com>
2021-05-23 19:38:05 +0200

    Agglomerative Clustering

--- 4.VisualizingHierarchies.py
+++ 4.VisualizingHierarchies.py
@@ -4,8 +4,8 @@
 Created on Fri May 21 20:34:16 2021
 
 @author: Alaisha Naidu
-Name: Visualizing Hierarchies
-Creds: DataCamp
+Name: Visualizing Hierarchies _ Hierarchical Clusterings
+Creds: DataCamp, KamKam (StackOverflow)
 Dataset: Eurovision 2016 results
 
 """
@@ -14,9 +14,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import linkage, dendrogram 
 
-eurovision = pd.read("/Users/user/Desktop/eurovision-2016-televoting.csv")
+eurovision = pd.read_csv('eurovision-2016.csv')
 country_names = eurovision['From country'].unique()
 toCountry = eurovision['To country'].unique()
 samples = np.empty((country_names.shape[0], toCountry.shape[0]))
 
+for i in range(eurovision.shape[0]):
+    toFound = False
+    toCount = 0
+    fromFound = False
+    fromCount = 0
+    while toFound == False:
+        if eurovision['To country'][i] == toCountry[toCount]:
+            toFound = True
+            while fromFound == False:
+                if eurovision['From country'][i] == country_names[fromCount]:
+                    fromFound = True
+                else:
+                    fromCount += 1
+        else:
+            toCount += 1
+    samples[fromCount,toCount] = eurovision['Televote Rank'][i]
+
+samples = np.round(samples, 0)
+
 mergings = linkage(samples, method = 'complete')
+
+
+#agglomerative hierarchical clustering means is a Bottom-Up clustering. With the bottom level having the most clusters
+#Each country being a cluster and then joining them together semantically
+
+dendrogram(mergings,
+           labels = country_names,
+           leaf_rotation=90,
+           leaf_font_size=6)
+plt.show()
